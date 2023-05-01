797f83329e82dbb3af78fdf85f30f222aae552d8
arhizx <mikhailrumov@gmail.com>
2023-05-01 18:21:50 +0300

    added counter component

--- src/App.js
+++ src/App.js
@@ -6,39 +6,40 @@ import React from 'react';
 import Button from './components/common/Button/Button.jsx';
 import Input from './components/common/Input/Input';
 import RadioButtons from './components/common/Input/RadioButtons';
-
+import Counter from './components/common/Counter/Counter';
 
 function App() {
   const [options, setOptions] = React.useState([
     { display: 'e-Money', value: 'e-money', active: false },
     { display: 'Cash on Delivery', value: 'cash', active: false },
-  ])
+  ]);
 
   // const findOptionByValue = (value) => {
   //   return options.find((option) => option.value === value);
   // }
 
   const handleRadioButtonClick = (value) => {
-    setOptions(options.map((option) => {
-      if (option.value === value) {
+    setOptions(
+      options.map((option) => {
+        if (option.value === value) {
+          return {
+            ...option,
+            active: true,
+          };
+        }
         return {
           ...option,
-          active: true,
+          active: false,
         };
-      }
-      return {
-        ...option,
-        active: false,
-      };
-    }))
-  }
+      })
+    );
+  };
 
   return (
     <div className="App">
       <Button type={BUTTON_TYPES.PRIMARY} text="SEE PRODUCT"></Button>
       <Button type={BUTTON_TYPES.SECONDARY} text="SEE PRODUCT"></Button>
       <Button type={BUTTON_TYPES.LIGHT} text="SHOP"></Button>
-
       <h1>YO CHECK THIS BASS OUT</h1>
       <h2>YO CHECK THIS BASS OUT</h2>
       <h3>YO CHECK THIS BASS OUT</h3>
@@ -47,10 +48,13 @@ function App() {
       <h6>YO CHECK THIS BASS OUT</h6>
       <p className="overline">YO CHECK THIS BASS OUT</p>
       <p className="sub_title">YO CHECK THIS BASS OUT</p>
-
       <Input type={INPUT_TYPES.TEXT} placeholder="Your Name" label="Name" />
-
-      <RadioButtons groupName="payments" options={options} clickHandler={handleRadioButtonClick} />
+      <RadioButtons
+        groupName="payments"
+        options={options}
+        clickHandler={handleRadioButtonClick}
+      />
+      <Counter quantity={6} />
     </div>
   );
 }
---
+++ src/components/common/Counter/Counter.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import styles from './Counter.module.scss';
+
+const Counter = ({ quantity }) => {
+  let [count, setCount] = useState(1);
+
+  const clickHandler = (operationType) => {
+    console.log(count);
+    if (operationType === 'plus' && count < quantity) {
+      setCount(count + 1);
+    }
+    if (operationType === 'minus' && count - 1 > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  return (
+    <div className={styles.counterWrapper}>
+      <span className={styles.minus} onClick={() => clickHandler('minus')}>
+        -
+      </span>
+      {count}
+      <span className={styles.plus} onClick={() => clickHandler('plus')}>
+        +
+      </span>
+    </div>
+  );
+};
+
+export default Counter;
