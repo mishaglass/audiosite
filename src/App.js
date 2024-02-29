c43573a2317e193c683189c8cf64942d29e0618c
mishaglass <128479384+mishaglass@users.noreply.github.com>
2024-03-01 00:55:48 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import { BUTTON_TYPES, INPUT_TYPES } from './constants';
 
 import React from 'react';
 
-import Button from './components/common/Button/Button.jsx';
-import Input from './components/common/Input/Input';
-import RadioButtons from './components/common/Input/RadioButtons';
-import Counter from './components/common/Counter/Counter';
+//import Button from './components/common/Button/Button.jsx';
+//import Input from './components/common/Input/Input';
+//import RadioButtons from './components/common/Input/RadioButtons';
+//import Counter from './components/common/Counter/Counter';
 import Header from './components/Header/Header.jsx';
 import NewProduct from './components/NewProduct/NewProduct.jsx';
 import LinkBar from './components/LinkBar/LinkBar.jsx';
